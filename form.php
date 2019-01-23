d0b549a9f8905a5712a6b5411d3298da728e4e46
Maria Kalusz <mkalusz1@gmail.com>
2019-01-23 15:24:34 -0800

    php

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_GET["name"];
+$email = $_GET["email"];
+$phone = $_GET["phone"];
+$state = $_GET["state"];
+
+echo "Name: " . $name;
+echo "<br>";
+echo "Email: " . $email;
+echo "<br>";
+echo "Phone: " . $phone;
+echo "<br>";
+echo "State: " . $state;
+    
+?>
